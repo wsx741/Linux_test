0704c90e98ccf95be8720a85ef8e136edebd6a76
zhaojing <2864567508@qq.com>
2025-09-17 14:26:25 +0800

    增加输入提示

--- src/exercise2/io_test01.cpp
+++ src/exercise2/io_test01.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int main()
 {
+    cout << "请输入一个整数：" << endl;
     int a;
     cin>>a;
     cout << "十进制：" << a << endl;
     cout << "十六进制：" << hex << a << endl;
     cout << "八进制：" << oct << a << endl;
 
+    cout << "请输入一个布尔值：" << endl;
     bool f1;
     cin>>f1;
     cout << f1 << endl;
     cout << f1 << boolalpha << f1 << endl;
    
+    cout << "请输入一个浮点数：" << endl;
     double b;
     cin>>b;
     cout.precision(3);
